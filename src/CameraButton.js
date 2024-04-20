7d18f75442ed768191e2bbf00070e34403d11439
uwu-alexandra <alexandra.sofronea27@gmail.com>
2024-04-20 22:51:14 +0300

    add spinner pretty

--- src/CameraButton.js
+++ src/CameraButton.js
@@ -2,11 +2,14 @@ import React, { useState, useRef } from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 import Constants from "expo-constants";
 import { Camera, CameraType } from "expo-camera";
+import Spinner from "react-native-loading-spinner-overlay";
 import * as MediaLibrary from "expo-media-library";
-import * as ImageManipulator from 'expo-image-manipulator';
+import * as ImageManipulator from "expo-image-manipulator";
 import Button from "./components/Button";
 import { auth, storage } from "../firebase.config";
 import { useEffect } from "react";
+import { colors } from "./Colors";
+
 
 export default function CameraButton() {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
@@ -14,11 +17,12 @@ export default function CameraButton() {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null);
+  const [spinner, setSpinner] = useState(false);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(status === 'granted');
+      setHasCameraPermission(status === "granted");
     })();
   }, []);
 
@@ -45,100 +49,105 @@ export default function CameraButton() {
     }
   };
 
-// Save the picture to the device's gallery and upload it to Firebase Storage
-const savePicture = async (fileUri, predictedClassName) => {
-  if (typeof fileUri !== 'string') {
-    console.error("Invalid file URI:", fileUri);
-    return null;
-  }
-
-  const resizedImageUri = await resizeImage(fileUri);
-  try {
-    const asset = await MediaLibrary.createAssetAsync(resizedImageUri);
-    const user = auth.currentUser;
-    const isGuest = user ? user.isAnonymous : false;
-    const userId = user.uid;
-    const response = await fetch(resizedImageUri);
-    const blob = await response.blob();
-
-    // Get the current timestamp
-    const timestamp = Date.now();
-    // Construct the image name with the predicted class name and timestamp
-    const imageName = `${predictedClassName}_${timestamp}.jpg`;
-
-    // Save the image to folder images/guests for guests or images/users/{userId} for registered users
-    const folderPath = isGuest ? `images/guests` : `images/users/${userId}`;
-    const storageRef = storage.ref(`${folderPath}/${imageName}`);
-    const snapshot = await storageRef.put(blob);
-    const imageUrl = await snapshot.ref.getDownloadURL();
-    // Reset the image state
-    setImage(null);
-    return imageUrl;
-  } catch (error) {
-    console.error("Upload failed:", error);
-    alert("Error", error.message);
-    return null;
-  }
-};
-
-// Send the image to the server for identification and wait for the prediction to finish
-const sendImageToServerAndWait = async (fileUri) => {
-  const predictedClassName = await sendImageToServer(fileUri);
-  if (predictedClassName) {
-    console.log("Image uploaded successfully with URL:", predictedClassName);
-    const imageUrl = await savePicture(fileUri, predictedClassName);
-  } else {
-    console.log("Failed to upload image.");
-  }
-};
-
-// Send the image to the server for identification
-const sendImageToServer = async (fileUri) => {
-  if (typeof fileUri !== 'string') {
-    console.error("Invalid file URI:", fileUri);
-    return null;
-  }
-
-  const formData = new FormData();
-  formData.append('file', {
-    uri: fileUri,
-    type: 'image/jpeg',
-    name: 'upload.jpg',
-  });
-  try {
-    const response = await fetch('https://cow-splendid-cicada.ngrok-free.app/predict', {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      console.log("Server response:", jsonResponse);
-      
-      // Get the predicted class name from the JSON response
-      return jsonResponse.predicted_class; // Return predicted class name
-    } else {
-      const errorResponse = await response.text();
-      throw new Error(`Failed to send file, status: ${response.status}`);
+  // Save the picture to the device's gallery and upload it to Firebase Storage
+  const savePicture = async (fileUri, predictedClassName) => {
+    if (typeof fileUri !== "string") {
+      console.error("Invalid file URI:", fileUri);
+      return null;
     }
-  } catch (error) {
-    console.error("Error sending file to server:", error);
-    alert("Error sending file to server:", error.message);
-    return null;
-  }
-};
 
-// Call the savePicture and sendImageToServerAndWait functions
-const saveAndIdentifyPicture = async () => {
-  if (image) {
-    await sendImageToServerAndWait(image);
-  } else {
-    console.log("No image available to send.");
-  }
-};
+    const resizedImageUri = await resizeImage(fileUri);
+    try {
+      const asset = await MediaLibrary.createAssetAsync(resizedImageUri);
+      const user = auth.currentUser;
+      const isGuest = user ? user.isAnonymous : false;
+      const userId = user.uid;
+      const response = await fetch(resizedImageUri);
+      const blob = await response.blob();
+
+      // Get the current timestamp
+      const timestamp = Date.now();
+      // Construct the image name with the predicted class name and timestamp
+      const imageName = `${predictedClassName}_${timestamp}.jpg`;
+
+      // Save the image to folder images/guests for guests or images/users/{userId} for registered users
+      const folderPath = isGuest ? `images/guests` : `images/users/${userId}`;
+      const storageRef = storage.ref(`${folderPath}/${imageName}`);
+      const snapshot = await storageRef.put(blob);
+      const imageUrl = await snapshot.ref.getDownloadURL();
+      // Reset the image state
+      setImage(null);
+      return imageUrl;
+    } catch (error) {
+      console.error("Upload failed:", error);
+      alert("Error", error.message);
+      return null;
+    }
+  };
+
+  // Send the image to the server for identification and wait for the prediction to finish
+  const sendImageToServerAndWait = async (fileUri) => {
+    setSpinner(true); // Show spinner
+    const predictedClassName = await sendImageToServer(fileUri);
+    if (predictedClassName) {
+      console.log("Image uploaded successfully with URL:", predictedClassName);
+      const imageUrl = await savePicture(fileUri, predictedClassName);
+    } else {
+      console.log("Failed to upload image.");
+    }
+    setSpinner(false); // Hide spinner
+  };
+
+  // Send the image to the server for identification
+  const sendImageToServer = async (fileUri) => {
+    if (typeof fileUri !== "string") {
+      console.error("Invalid file URI:", fileUri);
+      return null;
+    }
+
+    const formData = new FormData();
+    formData.append("file", {
+      uri: fileUri,
+      type: "image/jpeg",
+      name: "upload.jpg",
+    });
+    try {
+      const response = await fetch(
+        "https://cow-splendid-cicada.ngrok-free.app/predict",
+        {
+          method: "POST",
+          body: formData,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log("Server response:", jsonResponse);
+
+        // Get the predicted class name from the JSON response
+        return jsonResponse.predicted_class; // Return predicted class name
+      } else {
+        const errorResponse = await response.text();
+        throw new Error(`Failed to send file, status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error sending file to server:", error);
+      alert("Error sending file to server:", error.message);
+      return null;
+    }
+  };
+
+  // Call the savePicture and sendImageToServerAndWait functions
+  const saveAndIdentifyPicture = async () => {
+    if (image) {
+      await sendImageToServerAndWait(image);
+    } else {
+      console.log("No image available to send.");
+    }
+  };
 
   // If the user denies the camera permission, show a message
   if (hasCameraPermission === false) {
@@ -147,76 +156,87 @@ const saveAndIdentifyPicture = async () => {
 
   return (
     <View style={styles.container}>
-      {!image ? (
-        <Camera
-          style={styles.camera}
-          type={type}
-          ref={cameraRef}
-          flashMode={flash}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              paddingHorizontal: 30,
-            }}
-          >
-            <Button
-              title=""
-              icon="retweet"
-              onPress={() => {
-                setType(
-                  type === CameraType.back ? CameraType.front : CameraType.back
-                );
-              }}
-            />
-            <Button
-              onPress={() =>
-                setFlash(
-                  flash === Camera.Constants.FlashMode.off
-                    ? Camera.Constants.FlashMode.on
-                    : Camera.Constants.FlashMode.off
-                )
-              }
-              icon="flash"
-              color={flash === Camera.Constants.FlashMode.off ? "gray" : "#fff"}
-            />
-          </View>
-        </Camera>
+      <Spinner
+        visible={spinner}
+        textContent={"Loading identification..."}
+        textStyle={styles.spinnerTextStyle}
+        color={colors.focused} // This sets the spinner color
+        overlayColor="rgba(255, 255, 255, 0.8)" // Semi-transparent white background
+      />
+      {hasCameraPermission === false ? (
+        <Text>No access to camera</Text>
       ) : (
-        <Image
-          source={{ uri: image }}
-          style={styles.camera}
-        />
-      )}
+        <>
+          {!image ? (
+            <Camera
+              style={styles.camera}
+              type={type}
+              ref={cameraRef}
+              flashMode={flash}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  paddingHorizontal: 30,
+                }}
+              >
+                <Button
+                  title=""
+                  icon="retweet"
+                  onPress={() => {
+                    setType(
+                      type === CameraType.back
+                        ? CameraType.front
+                        : CameraType.back
+                    );
+                  }}
+                />
+                <Button
+                  onPress={() =>
+                    setFlash(
+                      flash === Camera.Constants.FlashMode.off
+                        ? Camera.Constants.FlashMode.on
+                        : Camera.Constants.FlashMode.off
+                    )
+                  }
+                  icon="flash"
+                  color={
+                    flash === Camera.Constants.FlashMode.off ? "gray" : "#fff"
+                  }
+                />
+              </View>
+            </Camera>
+          ) : (
+            <Image source={{ uri: image }} style={styles.camera} />
+          )}
 
-      <View style={styles.controls}>
-        {image ? (
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              paddingHorizontal: 50,
-            }}
-          >
-            <Button
-              title="Refă"
-              onPress={() => setImage(null)}
-              icon="retweet"
-            />
-            <Button
-              title="Identifică"
-              onPress={saveAndIdentifyPicture}
-              icon="check"
-            />
+          <View style={styles.controls}>
+            {image ? (
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  paddingHorizontal: 50,
+                }}
+              >
+                <Button
+                  title="Refă"
+                  onPress={() => setImage(null)}
+                  icon="retweet"
+                />
+                <Button
+                  title="Identifică"
+                  onPress={saveAndIdentifyPicture}
+                  icon="check"
+                />
+              </View>
+            ) : (
+              <Button onPress={takePicture} icon="camera" />
+            )}
           </View>
-        ) : (
-          <Button
-            onPress={takePicture}
-            icon="camera"
-          />
-        )}
-      </View>
+        </>
+      )}
     </View>
   );
 }
@@ -252,5 +272,9 @@ const styles = StyleSheet.create({
   },
   topControls: {
     flex: 1,
+  },
+  spinnerTextStyle: {
+    color: colors.focused,
+    fontSize: 20 // Optional: adjust the font size as needed
   },
 });
